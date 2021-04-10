ec53c6071d0b35feab1b9a2afc6252947b21b618
chasscepts <francischassobetta@gmail.com>
2021-04-10 07:53:40 +0100

    sum and count

--- answers.sql
+++ answers.sql
@@ -247,3 +247,47 @@ SELECT name, continent
   WHERE population / 3 > ALL (SELECT population FROM world w2 WHERE w1.continent = w2.continent AND NOT w1.name = w2.name);
 
 ----------------------- END SELECT WITHIN SELECT ------------------------
+
+------------------------------ SUM AND COUNT ----------------------------
+-- Show the total population of the world.
+SELECT SUM(population)
+  FROM world;
+
+-- List all the continents - just once each.
+SELECT DISTINCT continent
+  FROM world;
+
+-- Give the total GDP of Africa
+SELECT SUM(gdp)
+  FROM world
+  WHERE continent = 'Africa';
+
+-- How many countries have an area of at least 1000000
+SELECT COUNT(area)
+  FROM world
+  WHERE area >= 1000000;
+
+-- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+SELECT SUM(population)
+  FROM world
+  WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+-- For each continent show the continent and number of countries.
+SELECT continent, COUNT(continent)
+  FROM world
+  GROUP BY continent;
+
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+SELECT DISTINCT continent, COUNT(population)
+  FROM world
+  WHERE population >= 10000000
+  GROUP BY continent;
+
+-- List the continents that have a total population of at least 100 million.
+SELECT continent
+  FROM (SELECT continent, SUM(population) as sum FROM world GROUP BY continent) w
+  WHERE sum > 100000000;
+
+--------------------------- END SUM AND COUNT --------------------------
+
+
